a13978673f8874a73956b10bc91ad8a6149a46fe
T-Aizawa <aizawa.takamichi@gmail.com>
2018-04-21 17:28:59 +0900

    edit player
    移動、ジャンプ、地面の作成

---
+++ angel/Assets/Scripts/Peco.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Peco : MonoBehaviour
+{
+	// スピード
+	public float speedX = 5;
+	public float speedY = 7;
+
+	void Update ()
+	{
+		// 移動
+		float x = Input.GetAxisRaw ("Horizontal");
+		Vector2 direction = new Vector2 (x, 0).normalized;
+		GetComponent<Rigidbody2D>().velocity = direction * speedX;
+
+		// ジャンプ
+		Vector2 jump = new Vector2 (0, 1).normalized;
+
+		if (Input.GetKeyDown("space"))
+		{
+			GetComponent<Rigidbody2D>().velocity += jump * speedY;
+    }
+	}
+}
