35a9b4f16181c7c30c57646ae1599530899b8028
桧山正晃 <09s2035m@gmail.com>
2018-05-06 14:22:36 +0900

    ゲームマネージャー更新 ジャンプゲージプレハブ化

--- angel/Assets/Scripts/CGameManager.cs
+++ angel/Assets/Scripts/CGameManager.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CGameManager : MonoBehaviour {
+    public GameObject Peco;
+    public GameObject JumpGauge;
+
     public GameObject Ground;
     public GameObject Floor;
 
+    //GameManagerから参照する用
+    private GameObject refPeco;
+    private GameObject refGauge;
+
 	// Use this for initialization
 	void Start () {
-        float x = - 9.0f;
-        float y = - 4.5f;
+        CreateObject();
+
+	}
+	
+    void CreateObject()
+    {
+        CreateFloors();
+        CreateCharas();
+    }
+
+    void CreateFloors()
+    {
+        float x = -9.0f;
+        float y = -4.5f;
         for (; x < 10; x += 1.0f)
         {
             GameObject instance = Instantiate(Ground, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
@@ -17,15 +36,24 @@ public class CGameManager : MonoBehaviour {
         }
         for (x = 0.0f, y = -2.5f; y < 50f; y += 2.0f)
         {
-            GameObject instance = Instantiate(Floor, new Vector3(Random.Range(- 9f, 9f), y, 0f), Quaternion.identity) as GameObject;
+            GameObject instance = Instantiate(Floor, new Vector3(Random.Range(-9f, 9f), y, 0f), Quaternion.identity) as GameObject;
             instance.transform.SetParent(GetComponent<Transform>());
         }
+    }
 
-	}
-	
+    void CreateCharas()
+    {
+        refPeco = Instantiate(Peco, new Vector3(0f, -3f, 0f), Quaternion.identity) as GameObject;
+        refPeco.name = Peco.name;
+
+        refGauge = Instantiate(JumpGauge, new Vector3(-11f, 6f, 15f), Quaternion.identity) as GameObject;
+        refGauge.name = JumpGauge.name;
+//        refGauge.transform.parent = 
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        refGauge.GetComponent<Gauge>().JumpBarVisualize(Peco.GetComponent<Peco>().GetJumpEnergyRate());
 	}
 
 }
---
+++ angel/Assets/Scripts/Gauge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gauge : MonoBehaviour {
+    private float BarSize;
+    private GameObject GaugeFront;
+
+	// Use this for initialization
+	void Start () {
+        BarSize = GetComponent<Transform>().localScale.x;
+        GaugeFront = GetComponent<Transform>().Find("JumpGauge_front").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void JumpBarVisualize(float rate)
+    {
+        GaugeFront.transform.localScale.Set(rate, 1.0f, 1.0f);
+        //位置を左に合わせる
+        Vector3 tmpPosition = GaugeFront.transform.localPosition;
+        tmpPosition.x -= BarSize * (1.0f - rate);
+        GaugeFront.transform.localPosition = tmpPosition;
+    }
+}
