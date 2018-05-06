5d9c28329f6373ea17d5050e5028d402f50bf1d9
T-Aizawa <aizawa.takamichi@gmail.com>
2018-05-06 15:20:18 +0900

    タイトル追加
    Titleシーンの追加。
    StartButtonの作成。

---
+++ angel/Assets/Scripts/ClickStartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickStartButton : MonoBehaviour {
+	public void StartButton() {
+		Debug.Log("mannse-");
+		SceneManager.LoadScene("play");
+	}
+}
