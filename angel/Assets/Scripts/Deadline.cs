dd25a80414417913291f827cdab94241855424ed
T-Aizawa <aizawa.takamichi@gmail.com>
2018-05-12 13:43:13 +0900

    Deadline修正
    レイヤーがPecoの時、タイトル画面へ遷移する様に修正

--- angel/Assets/Scripts/Deadline.cs
+++ angel/Assets/Scripts/Deadline.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Deadline : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		Destroy(other.gameObject);
+		// レイヤー名の取得
+		string layerName = LayerMask.LayerToName(other.gameObject.layer);
+
+		// レイヤーが足場のとき
+		if( layerName == "Foot")
+        {
+            // 足場の削除
+			Destroy(other.gameObject);
+        }
+
+		// レイヤーがペコのとき
+		if( layerName == "Peco")
+        {
+            // GameOverのシーンへ移行
+			SceneManager.LoadScene("play");
+        }
 	}
 }
